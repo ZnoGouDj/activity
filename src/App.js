ab257dee6d96da9dbf98f56386ffdc071c559c24
Egor Sorokin <znogoud3@gmail.com>
2023-02-18 00:38:06 +0100

    add basic layout and random action generation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // assumes you have a CSS file for styling
 
-function App() {
+function GameBoard() {
+  const words = ['speak', 'show', 'draw'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="game-board">
+      <div className="field-grid">
+        {Array.from({ length: 49 }, (_, i) => {
+          const isStartOrFinish = i !== 0 && i !== 48;
+          const word = words[Math.floor(Math.random() * words.length)];
+          const fieldClass = word && isStartOrFinish ? `field ${word}` : 'field';
+          return (
+            <div className={fieldClass} key={i}>
+              {isStartOrFinish ? word : ''}
+            </div>
+          );
+        })}
+      </div>
+      <div className="card-slots">
+        {Array.from({ length: 3 }, (_, i) => (
+          <div className="card" key={i}>
+            <h2>Activity</h2>
+            <p className='difficulty'>{5 - i}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
-export default App;
+export default GameBoard;
